dd22a7fb355eb4620d8fceb1c69a41d01f9a42f6
Emmett Young <emmettyoung92@gmail.com>
2019-04-20 21:10:33 +0800

    Add test for one path and two dead ends for routing service

--- backend/src/test/java/com/mettyoung/railwayroutingservice/routing/RailwayRoutingServiceTest.java
+++ backend/src/test/java/com/mettyoung/railwayroutingservice/routing/RailwayRoutingServiceTest.java
@@ -14,7 +14,7 @@ import static org.hamcrest.Matchers.*;
 public class RailwayRoutingServiceTest {
 
     @Test
-    public void should_provide_a_route_from_start_to_end_given_one_path_of_3_stations() {
+    public void should_provide_a_route_from_start_to_end_given_one_straight_path_of_3_stations() {
         Station one = new Station("one");
         Station two = new Station("two");
         Station three = new Station("three");
@@ -26,6 +26,42 @@ public class RailwayRoutingServiceTest {
                 .addConnection(two, three);
 
         RailwayRoutingService railwayRoutingService = new RailwayRoutingService(railway);
+        List<RailwayPath> paths = railwayRoutingService.computePaths("one", "three");
+
+        assertThat(paths, hasSize(1));
+        assertThat(paths.get(0).getRailwayEdges(), hasSize(2));
+        assertThat(paths.get(0).getRailwayEdges(), contains(
+                allOf(
+                        hasProperty("startStation", sameInstance(one)),
+                        hasProperty("endStation", sameInstance(two))
+                ),
+                allOf(
+                        hasProperty("startStation", sameInstance(two)),
+                        hasProperty("endStation", sameInstance(three))
+                )
+        ));
+    }
+
+    @Test
+    public void should_provide_a_route_from_start_to_end_given_a_graph_with_two_dead_ends_and_one_path() {
+        Station one = new Station("one");
+        Station two = new Station("two");
+        Station three = new Station("three");
+        Station dangledOne = new Station("dangledOne");
+        Station dangledTwo = new Station("dangledTwo");
+        Railway railway = new Railway()
+                .addStation(one)
+                .addStation(two)
+                .addStation(three)
+                .addStation(dangledOne)
+                .addStation(dangledTwo)
+                .addConnection(one, two)
+                .addConnection(two, three)
+                .addConnection(one, dangledOne)
+                .addConnection(two, dangledTwo);
+
+        RailwayRoutingService railwayRoutingService = new RailwayRoutingService(railway);
+
         List<RailwayPath> paths = railwayRoutingService.computePaths("one", "three");
 
         assertThat(paths, hasSize(1));
