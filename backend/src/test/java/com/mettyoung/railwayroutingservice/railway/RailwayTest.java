a371f3fc2609f3f7456624612958898f1adb8f4b
Emmett Young <emmettyoung92@gmail.com>
2019-04-20 15:48:49 +0800

    Railway should be able to get two stations from railway given two stations having the same station name

--- backend/src/test/java/com/mettyoung/railwayroutingservice/railway/RailwayTest.java
+++ backend/src/test/java/com/mettyoung/railwayroutingservice/railway/RailwayTest.java
@@ -1,19 +1,35 @@
 package com.mettyoung.railwayroutingservice.railway;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 public class RailwayTest {
 
     @Test
     public void should_be_able_to_get_one_station_from_railway_given_one_station_in_a_station_name() {
         Station one = new Station("one", "group");
+
         Railway railway = new Railway();
         railway.addStation(one);
 
-        Assert.assertThat(railway.findStations("group"), hasSize(1));
-        Assert.assertThat(railway.findStations("group"), hasItem(sameInstance(one)));
+        assertThat(railway.findStations("group"), hasSize(1));
+        assertThat(railway.findStations("group"), hasItem(sameInstance(one)));
+    }
+
+    @Test
+    public void should_be_able_to_get_two_stations_from_railway_given_two_stations_having_the_same_station_name() {
+        Station one = new Station("one", "group");
+        Station two = new Station("two", "group");
+        Station three = new Station("three", "another-group");
+
+        Railway railway = new Railway();
+        railway.addStation(one)
+                .addStation(two)
+                .addStation(three);
+
+        assertThat(railway.findStations("group"), hasSize(2));
+        assertThat(railway.findStations("group"), hasItems(sameInstance(one), sameInstance(two)));
     }
 }
