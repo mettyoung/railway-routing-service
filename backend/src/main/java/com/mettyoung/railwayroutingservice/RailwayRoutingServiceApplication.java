699a86e3e8c1563c13a1c4f522ecb6e7dc901d37
Emmett Young <emmettyoung92@gmail.com>
2019-04-20 14:04:33 +0800

    Initialize Spring Boot application

---
+++ backend/src/main/java/com/mettyoung/railwayroutingservice/RailwayRoutingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mettyoung.railwayroutingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayRoutingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayRoutingServiceApplication.class, args);
+    }
+
+}
