35a290a792854717df2306f882303d04b0ca7800
Emmett Young <emmettyoung92@gmail.com>
2019-04-21 15:17:20 +0800

    Build railway on startup

---
+++ backend/src/main/java/com/mettyoung/railwayroutingservice/DomainConfig.java
@@ -0,0 +1,29 @@
+package com.mettyoung.railwayroutingservice;
+
+import com.mettyoung.railwayroutingservice.common.parser.CsvToBeanParser;
+import com.mettyoung.railwayroutingservice.railway.Railway;
+import com.mettyoung.railwayroutingservice.railway.RailwayFactory;
+import com.mettyoung.railwayroutingservice.railway.Station;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Configuration
+public class DomainConfig {
+
+    @Value("${railway.csvPath:classpath:StationMap.csv}")
+    private String csvPath;
+
+    @Bean
+    public Railway buildRailway() throws IOException {
+        File file = ResourceUtils.getFile(csvPath);
+        List<Station> stations = CsvToBeanParser.parseCsvToBean(file, Station.class);
+
+        return RailwayFactory.buildRailway(stations);
+    }
+}
