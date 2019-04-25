891420963801304cfa59bfc66408755efd12ecbf
Emmett Young <emmettyoung92@gmail.com>
2019-04-25 21:47:49 +0800

    Add initial UI layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-4">
+      <Row>
+        <h2 className="m-auto mx-md-2">Singapore Railway System</h2>
+      </Row>
+      <Row className="mt-4">
+        <Col md={4} xs={12}>
+          <Form>
+            <Form.Group controlId="formOriginStation">
+              <Form.Label>Origin Station</Form.Label>
+              <Form.Control as="select">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Form.Control>
+              <Form.Text className="text-muted">
+                Please choose your origin station.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formTargetStation">
+              <Form.Label>Target Station</Form.Label>
+              <Form.Control as="select">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Form.Control>
+              <Form.Text className="text-muted">
+                Please choose your destination station.
+              </Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Suggest Routes
+            </Button>
+          </Form>
+        </Col>
+        <Col className="my-4 my-md-0">
+          <Card>
+            <Card.Body className="m-xl-2">
+              <Card.Title>Stations Travelled: 8</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Route: ('CC21', 'CC20', 'CC19', 'DT9', 'DT10', 'DT11', 'DT12',
+                'DT13', 'DT14')</Card.Subtitle>
+              <Card.Text>
+                <ul>
+                  <li>Take CC line from Holland Village to Farrer Road</li>
+                  <li>Take CC line from Farrer Road to Botanic Gardens</li>
+                  <li>Change from CC line to DT line</li>
+                  <li>Take DT line from Botanic Gardens to Stevens</li>
+                  <li>Take DT line from Stevens to Newton</li>
+                  <li>Take DT line from Newton to Little India</li>
+                  <li>Take DT line from Little India to Rochor</li>
+                  <li>Take DT line from Rochor to Bugis</li>
+                </ul>
+              </Card.Text>
+              <Card.Link href="#">Previous</Card.Link>
+              <Card.Link href="#">Next</Card.Link>
+              <span className="float-right text-muted small">Page 1 of 3</span>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
